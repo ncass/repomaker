c389afd45ff25ed79422499269361b19b398145b
nick cassara <nickcassara3@gmail.com>
2020-08-15 15:22:53 -0400

    connects to git and makes desktop folder

--- repo.py
+++ repo.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import os
 
 #https://github.com/
 
 #setup webdriver
 class r:
-    def __init__(self, url):
+    def __init__(self, url, rname, description):
         self.url = url
+        self.rname = rname
+        self.description = description
         self.driver = webdriver.Chrome("/usr/local/bin/chromedriver")
     
     #method to take github url and setup repo
@@ -15,9 +18,23 @@ class r:
         self.usrname = self.driver.find_element_by_id("login_field").send_keys("/")
         self.pwd = self.driver.find_element_by_id("password").send_keys("/")
         self.signin = self.driver.find_element_by_name("commit").click()
+        self.create = self.driver.find_element_by_xpath("/html/body/div[4]/div/aside[1]/div[2]/div[1]/div/h2/a").click()
+        self.repo = self.driver.find_element_by_id("repository_name").send_keys(self.rname)
+        self.desc = self.driver.find_element_by_id("repository_description").send_keys(self.description)
+        #self.submit = self.driver.find_element_by_xpath("/html/body/div[4]/main/div/form/div[4]/button").click()
+    
+    def quit(self):
+        self.driver.close()
 
+    #method to create a local folder to connect to this repo
+    #def local(self)
 
-
-        
-test = r("https://github.com/login")
-test.go()
+if __name__ == "__main__":
+    nr = r("https://github.com/login", "pi-projects", "gpio scripts for pi")
+    nr.go()
+    nr.quit()
+    os.chdir("/Users/nicholascassara/Desktop")
+    os.system("mkdir "+ nr.rname)
+    os.chdir("/Users/nicholascassara/Desktop/"+nr.rname)
+    os.system("git init")
+    
