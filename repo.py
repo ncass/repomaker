380b90290d8b6a5dc493f5ab6c7e8b2cc45a0328
nick cassara <nickcassara3@gmail.com>
2020-08-15 14:14:09 -0400

    signs in to github

--- repo.py
+++ repo.py
@@ -9,11 +9,15 @@ class r:
         self.url = url
         self.driver = webdriver.Chrome("/usr/local/bin/chromedriver")
     
+    #method to take github url and setup repo
     def go(self):
         self.driver.get(self.url)
+        self.usrname = self.driver.find_element_by_id("login_field").send_keys("/")
+        self.pwd = self.driver.find_element_by_id("password").send_keys("/")
+        self.signin = self.driver.find_element_by_name("commit").click()
 
 
 
         
-test = r("https://github.com/")
+test = r("https://github.com/login")
 test.go()
